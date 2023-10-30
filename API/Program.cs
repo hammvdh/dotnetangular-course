fe28cd0126a096bd17f4ffc8685a1b3904e0e49b
Hammadh Arquil <hammadhy11@gmail.com>
2023-10-30 15:22:50 +0530

    Create JWT logic

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,6 @@
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,18 +11,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
-
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
-
 app.UseAuthorization();
-
 app.UseHttpsRedirection();
-
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
-
 app.MapControllers();
-
 app.Run();
